ca443f91f0efc4385d722ce4bd4dfc7c0b802a1d
MaxWalker20 <17007433@hope.ac.uk>
2022-07-31 14:15:10 +0100

    Cleaned up code, allowed page to be read by server

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 
 var app = express();
+app.use('/images', express.static(__dirname+'/images'));
+app.use('/openseadragon', express.static(__dirname+'/openseadragon'));
 app.get('/', function (req, res) {
-  app.use(express.static('images'));
   res.sendFile(path.join(__dirname+'/main.html'));
 });
 app.listen(3000, function () {
--- my-module.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  oobit: '🌎🌠'
-}
