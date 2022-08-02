da078dc88d73e3c698476f29b94fa256312efb8c
MaxWalker20 <17007433@hope.ac.uk>
2022-08-02 14:39:47 +0100

    Programatically created dropdownBox

--- public/client.js
+++ public/client.js
@@ -2,6 +2,7 @@ const button = document.getElementById('loadButton');
 const viewerId = 'viewer';
 
 
+
 function loadImage(imagePath) {
   const viewer = document.createElement('div');
   viewer.id = viewerId;
@@ -16,6 +17,28 @@ function loadImage(imagePath) {
       url:  imagePath
     }
   });
+}
+
+function buildDropDown() {
+  var myParent = document.getElementById('controls');
+
+  //Create array of options to be added
+  var array = ["Volvo","Saab","Mercades","Audi"];
+
+  //Create and append select list
+  var selectList = document.createElement("select");
+  selectList.id = "mySelect";
+  myParent.appendChild(selectList);
+
+  //Create and append the options
+  for (var i = 0; i < array.length; i++) {
+      var option = document.createElement("option");
+      option.value = array[i];
+      option.text = array[i];
+      selectList.appendChild(option);
+  }
+
+
 }
 
 button.addEventListener('click', function(e) {
@@ -42,3 +65,7 @@ button.addEventListener('click', function(e) {
       console.log(error);
     });
 });
+
+window.onload = (event) => {
+  buildDropDown();
+};
